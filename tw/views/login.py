28e00ec77eb0f86a595e72e4a87a5e2be0d36241
andreijuravle <andrei.juravle@info.uaic.ro>
2016-05-29 16:54:33 +0300

    add login backend

--- tw/views/login.py
+++ tw/views/login.py
@@ -4,7 +4,8 @@ from pyramid.response import Response
 from pyramid.renderers import render_to_response
 import json
 from passlib.hash import sha256_crypt
-
+from ..models.meta import DBSession
+from ..models.user import User
 @view_defaults(route_name = 'login')
 class Login(object):
     def __init__(self,request):
@@ -18,11 +19,22 @@ class Login(object):
     @view_config(request_method = 'POST', renderer = 'json')
     def postLoginPage(self):
         user = {'email': self.request.params['email'], 'password': self.request.params['password']}
-        # error = {"email_required": True, "password_required": True, "incorrect_credentials": True}
-        # return render_to_response('templates/login.jinja2', error, request = self.request)
+        credentials_db = {}
+        try:
+            credentials_db = DBSession.query(User).filter(User.mail == user['email'], User.parola == user['password']).first().as_dict()
+            print(credentials_db)
+        except AttributeError:
+            error = {"email_required": False, "password_required": False, "incorrect_credentials": False}
+            if len(user['email']) == 0:
+                error['email_required'] = True
+            if len(user['password']) == 0:
+                error['password_required'] = True
+            if (len(user['email']) > 0) and (len(user['password']) > 0):
+                error['incorrect_credentials'] = True
+            return render_to_response('templates/login.jinja2', error, request = self.request)
+        
         self.request.session['email'] = user['email']
-        #self.request.session['id'] = existing_user[0]['_id']
-        #self.request.session['follow'] = existing_user[0]['follow']
+        self.request.session['id_user'] = credentials_db['id_user']
         token = sha256_crypt.encrypt(self.request.session.get_csrf_token())
         response = HTTPFound(location = self.request.route_url('home'))
         response.set_cookie('XSRF-TOKEN', value=token)
